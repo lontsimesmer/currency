e9410dbeae1e85f62afc669c34ac8a7ac8b5a2dc
LONTSI TCHOUPOU Nathanaël Mesmer <tchopounathanael@gmail.com>
2023-02-17 23:53:53 +0100

    other functions added

--- src/Currency.jsx
+++ src/Currency.jsx
@@ -9,53 +9,90 @@ export default function Currency() {
   const [to, setTo] = useState("eur");
   const [currency, setCurrency] = useState("usd");
   const [total, setTotal] = useState(0);
-  const [wallet, setWallet] = useState({
-    usd: { amount: 100 },
-    eur: { amount: 500 },
-    xaf: { amount: 1000 },
-  });
+  const [wallet, setWallet] = useState([
+    { sign: "usd", amount: 100 },
+    { sign: "eur", amount: 500 },
+    { sign: "xaf", amount: 1000 },
+  ]);
   const [rates, setRates] = useState();
   const [deposit, setDeposit] = useState("usd");
   const [options, setOptions] = useState([]);
 
   useEffect(() => {
     fetch(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`
-    )
-      .then((res) => res.json())
-      .then((res) => setOptions([...Object.keys(res[from])]));
-  }, [from]);
-
-  useEffect(() => {
-    fetch(
       `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
     )
       .then((res) => res.json())
-      .then((res) => setRates([res[currency]]))
-      .then(() => {
-        console.log(rates);
-        let tempTotal = 0;
-        Object.keys(wallet)?.forEach((el) => {
-          tempTotal += wallet[el].amount / rates.el;
-          console.log(wallet[el].amount, el, rates.el);
-        });
-        setTotal(tempTotal);
+      .then((res) => {
+        setRates(res.usd);
+        setOptions([...Object.keys(res[currency])]);
       });
-  }, [wallet, currency]);
+  }, [currency]);
+
+  // useEffect(() => {
+  //   fetch(
+  //     `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((res) => setRates([res[currency]]))
+  //     .then(() => {
+  //       let usdValue = 0;
+  //       let eurValue = 0;
+  //       let xafValue = 0;
+  //       let getTotal = 0;
+  //       Object.keys(wallet)?.forEach((el) => {
+  //         usdValue += wallet[el].amount * rates.el;
+  //         eurValue += wallet[el].amount * rates.el;
+  //         xafValue += wallet[el].amount * rates.el;
+  //         getTotal += wallet[el].amount;
+  //         /* console.log(wallet[el].amount, el, rates.el); */
+  //       });
+  //       setWallet(usdValue, eurValue, xafValue);
+  //       setTotal(getTotal);
+  //     });
+  // }, [wallet, currency]);
+
+  function handleConvert() {
+    const rate = rates;
+    const Results = (input / rate[from]) * rate[to];
+    const holder = wallet;
+    const newWallet = holder.map((curr) => {
+      if (curr.sign === from) {
+        curr.amount -= input;
+      }
+      if (curr.sign === to) {
+        return curr;
+      }
+    });
+
+    newWallet[from].amount -= input;
+    newWallet[to].amount += Results;
+    setWallet(newWallet);
+  }
+
+  // console.log("this wallet", wallet);
+
+  function handleConfirm() {}
 
   if (options.length > 0) {
     return (
       <div className="Currency">
         <h1>CURRENCY EXCHANGE</h1>
         <div className="container">
-          <div className="converter">
+          <form
+            className="converter"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleConvert();
+            }}
+          >
             <h2>Converter</h2>
             <div className="input-field">
               <h3>Amount:</h3>
               <input
-                type="text"
                 id="amount"
-                onChange={(e) => setInput(e.target.value)}
+                type="number"
+                onChange={(e) => setInput(+e.target.value)}
               />
             </div>
 
@@ -64,38 +101,40 @@ export default function Currency() {
               <ReactDropdown
                 className="dropdown"
                 options={options}
+                value={from.toUpperCase()}
+                placeholder="From"
                 onChange={(e) => {
                   setFrom(e.value);
                 }}
-                value={from.toUpperCase()}
-                placeholder="From"
               />
               <h4>To</h4>
               <ReactDropdown
                 className="dropdown"
                 options={options}
+                value={to.toUpperCase()}
+                placeholder="To"
                 onChange={(e) => {
                   setTo(e.value);
                 }}
-                value={to.toUpperCase()}
-                placeholder="To"
               />
             </div>
-            <button
-              type="button"
-              onClick={() => {
-                /* convert(); */
-              }}
-            >
-              Convert
-            </button>
-            {/* <h3>Converted Amount : {output.toFixed(2)}</h3> */}
-          </div>
-          <div className="deposit">
+            <button type="submit">Convert</button>
+          </form>
+          <form
+            className="deposit"
+            onSubmit={(e) => {
+              e.prevenDefault();
+              handleConfirm();
+            }}
+          >
             <h2>Banque</h2>
             <div className="input-field">
               <h3>Amount:</h3>
-              <input id="amount" type="text" />
+              <input
+                id="amount"
+                type="number"
+                onChange={(e) => setInput(+e.target.value)}
+              />
             </div>
             <div className="select-options">
               <h3>Deposit currency:</h3>
@@ -116,7 +155,7 @@ export default function Currency() {
             >
               Confirm
             </button>
-          </div>
+          </form>
           <div className="wallet">
             <h2>My Wallet</h2>
             <div className="select">
@@ -131,9 +170,9 @@ export default function Currency() {
               />
             </div>
             <div className="currencies">
-              {Object.keys(wallet).map((el) => (
+              {wallet.map((el) => (
                 <p key={el}>
-                  {el.toUpperCase()}: {wallet[el].amount}
+                  {el.sign.toUpperCase()}: {el.amount}
                 </p>
               ))}
             </div>
